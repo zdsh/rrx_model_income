f81f890f64d68bf550f54b42bbe1e8e0de677200
liben <282969053@qq.com>
2016-05-19 10:53:37 +0800

    修改了一些求均值，中位数的方法

--- feature/pbc/credit_card_info.py
+++ feature/pbc/credit_card_info.py
@@ -14,46 +14,55 @@ sys.setdefaultencoding('utf-8')
 
 from utils import mongo_connect
 import pandas as pd
+import numpy as np
 
 SPACE='    '
 def get_credit_card_info_feature(credit_card_detail):
-    feature={}
-    mean = 0
+
+    feature={
+        'AmountMean':-99,
+        'AmountMedian':-99,
+        'AmountMax':-99,
+        'AmountMin':-99
+        }
+
     amount_list = []
-    for k,v in credit_card_detail.items():
-        card_detail = v
-        if card_detail['CURRENCY'] == '人民币' and  card_detail['STATUS']=='1':
+    if credit_card_detail:
+        for k,v in credit_card_detail.items():
+            card_detail = v
+            if card_detail['CURRENCY'] == '人民币' and  card_detail['STATUS']=='1':
+                amount_list.append(int(card_detail['AMOUNT']))
 
-            amount_list.append(int(card_detail['AMOUNT']))
-
-    #print(feature)
-    df = pd.DataFrame(amount_list)
-    feature['AmountMean'] = str(df.mean()).split('\n')[0].split(SPACE)[1]
-    feature['AmountMedian'] = str(df.median()).split('\n')[0].split(SPACE)[1]
-    feature['AmountMax'] = str(df.max()).split('\n')[0].split(SPACE)[1]
-    feature['AmountMin'] = str(df.min()).split('\n')[0].split(SPACE)[1]
+        myarray = np.array(amount_list)
+        if len(amount_list):
+            feature['AmountMean'] = np.mean(myarray)
+            feature['AmountMedian'] = np.median(myarray)
+            feature['AmountMax'] = np.max(myarray)
+            feature['AmountMin'] = np.min(myarray)
+    #print feature
 
     return feature
 
 
-# if __name__ == '__main__':
-#     db=mongo_connect.get_mongo_db('rrx_xwdb', '10.10.159.15', 27017, 'rrx_xw', 'rrx_xw_pass')
-#     collection = db['xw_users']
-#
-#     applycode_list=[
-#         'A20160304864714',
-#         'A20160307866631'
-#         ]
-#     pbc_info = {}
-#
-#     for user in collection.find({'_id':{'$in':applycode_list}}):
-#
-#         if 'PBC' in user:
-#             pbc_info=user['PBC']
-#             credit_card_detail_info = {}
-#             if 'AP_PBC_CREDIT_CARD_DETAIL' in pbc_info:
-#                 credit_card_detail_info=pbc_info['AP_PBC_CREDIT_CARD_DETAIL']
-#                 feature = get_credit_card_info_feature(credit_card_detail_info)
-#
-#         print user['_id'],feature
-#         #break
+if __name__ == '__main__':
+
+    db=mongo_connect.get_mongo_db('rrx_xwdb', '10.10.159.15', 27017, 'rrx_xw', 'rrx_xw_pass')
+    collection = db['xw_users']
+
+    applycode_list=['M20160411054092','M20160321045256']
+
+    #apply_file = open('/Users/liben/PycharmProjects/Model_Analysis/My_Data_Analysis/datas/','r')
+
+    pbc_info = {}
+
+    for user in collection.find({'_id':{'$in':applycode_list}}):
+
+        if 'PBC' in user:
+            pbc_info=user['PBC']
+            credit_card_detail_info = {}
+            if 'AP_PBC_CREDIT_CARD_DETAIL' in pbc_info:
+                credit_card_detail_info=pbc_info['AP_PBC_CREDIT_CARD_DETAIL']
+                feature = get_credit_card_info_feature(credit_card_detail_info)
+
+        print user['_id'],feature
+        #break
--- feature/pbc/pbc_feature.py
+++ feature/pbc/pbc_feature.py
@@ -11,11 +11,14 @@ def get_pbc_feature_dic(pbc_info):
     feature={}
     credit_card_info={}
 
+    #获取信用卡明细
     if 'AP_PBC_CREDIT_CARD_DETAIL' in pbc_info:
         credit_card_info=pbc_info['AP_PBC_CREDIT_CARD_DETAIL']
 
+    #提取信用卡的特征
     credit_card_feature=get_credit_card_info_feature(credit_card_info)
     for k in credit_card_feature:
         feature[k]=credit_card_feature[k]
-    print(feature)
+    #print(feature)
     return feature
+
